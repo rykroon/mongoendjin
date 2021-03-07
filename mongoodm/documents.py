80a0552b4c67aab70aae4d7a7aad2e698b456fa9
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-07 15:46:26 -0500

    added logic for getting clients, dbs, and collections

--- mongoodm/documents.py
+++ mongoodm/documents.py
@@ -1,10 +1,14 @@
-from .connections import client
+from .connections import get_client, DEFAULT_CLIENT_NAME, DEFAULT_DB_NAME
 from .fields import Field, ObjectIdField
 
 
 class Document:
 
-    _meta = {}
+    _meta = {
+        'client_name': DEFAULT_CLIENT_NAME,
+        'db_name': DEFAULT_DB_NAME,
+        'collection_name': None
+    }
 
     _id = ObjectIdField()
 
@@ -30,31 +34,50 @@ class Document:
 
     @classmethod
     def _get_fields(cls):
-        for attr in dir(cls):
-            if attr.startswith('__'):
-                continue
+        if not hasattr(cls, '_fields'):
+            cls._fields = []
+            for attr in dir(cls):
+                if attr.startswith('__'):
+                    continue
 
-            value = getattr(cls, attr, None)
-            if not isinstance(value, Field):
-                continue
+                value = getattr(cls, attr, None)
+                if not isinstance(value, Field):
+                    continue
 
-            yield value
+                cls._fields.append(value)
+        return cls._fields
+
+    def _get_client(self):
+        client_name = self._meta.get('client_name')
+        return get_client(client_name)
+
+    def _get_db(self):
+        db_name = self._meta.get('db_name')
+        return self._get_client()[db_name]
+
+    def _get_collection(self):
+        collection_name = self._get_collection_name()
+        return self._get_db()[collection_name]
+
+    def _get_collection_name(self):
+        collection_name = self._meta.get('collection_name')
+        if collection_name is None:
+            collection_name = self.__class__.__name__.lower()
+        return collection_name
 
     @property
     def pk(self):
         return self._id
 
-    def _get_collection(self):
-        return client['test_db'][self._get_collection_name()]
-
-    def _get_collection_name(self):
-        collection_name = self._meta.get('collection_name')
-        if collection_name is None:
-            collection_name = self.__class__.__name__.tolower()
-        return collection_name
-
-    def to_dict(self):
-        return {f.name: getattr(self, f.name) for f in self._get_fields()}
+    def to_dict(self, inc=None, exc=None):
+        d = {}
+        for f in self._get_fields():
+            if inc and f.name not in inc:
+                continue
+            if exc and f.name in exc:
+                continue
+            d[f.name] = getattr(self, f.name)
+        return d
 
     def save(self, validate=True, clean=True):
         if clean:
@@ -69,7 +92,7 @@ class Document:
         return self._insert()
 
     def _insert(self):
-        result = self._get_collection().insert_one(self.to_dict())
+        result = self._get_collection().insert_one(self.to_dict(exc=['_id']))
         self._id = result.inserted_id
 
     def _update(self):
