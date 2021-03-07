b63774914cd7179e79ad1aaa29d6051e767a56b1
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-07 15:07:16 -0500

    added db_name to connection

--- mongoodm/connections.py
+++ mongoodm/connections.py
@@ -1,8 +1,18 @@
 from pymongo import MongoClient
 
 client = None
+db_name = None
 
-def connect(**kwargs):
+
+def connect(db_name, **kwargs):
     global client
+    global db
+    db = db
     client = MongoClient(**kwargs)
-    return client()
+    return client
+
+
+def get_db():
+    global client
+    global db_name
+    return client[db_name]
