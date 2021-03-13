9a03de0ff6a80dd7ba04bd19b5ccad04aebbd108
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-13 11:01:01 -0500

    added CachedProperty decorator

--- mongoodm/utils.py
+++ mongoodm/utils.py
@@ -1,5 +1,25 @@
 
 
+Undefined = object()
+
+
+class CachedProperty:
+
+    def __init__(self, func):
+        self.func = func
+        self.name = '_{}'.format(func.__name__)
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+
+        value = getattr(instance, self.name, Undefined)
+        if value is Undefined:
+            value = self.func(instance)
+            setattr(instance, self.name, value)
+        return value
+
+
 def process_key(k, v):
     if '__' not in k:
         return {k: v}
