d976e35962554d68a48b963c3cfe22c82f722ac2
Ryan Kroon <rykroon.tech@gmail.com>
2021-04-01 20:29:38 -0400

    registered lookups to fields

--- mongopy/lookups.py
+++ mongopy/lookups.py
@@ -1,4 +1,4 @@
-
+from .fields import Field
 
 
 class Lookup:
@@ -18,34 +18,40 @@ class Lookup:
         return {self.mongo_op: value}
 
     def as_mongo(self):
-        return {self.lhs: self.process_rhs()}
+        return {self.lhs.name: self.process_rhs()}
 
 
+@Field.register_lookup
 class Exact(Lookup):
     lookup_name = 'exact'
     mongo_op = '$eq'
     
 
+@Field.register_lookup
 class GreaterThan(Lookup):
     lookup_name = 'gt'
     mongo_op = '$gt'
 
 
+@Field.register_lookup
 class GreaterThanOrEqual(Lookup):
     lookup_name = 'gte'
     mongo_op = '$gte'
 
 
+@Field.register_lookup
 class LessThan(Lookup):
     lookup_name = 'lt'
     mongo_op = '$lt'
 
 
+@Field.register_lookup
 class LessThanOrEqual(Lookup):
     lookup_name = 'lte'
     mongo_op = '$lte'
 
 
+@Field.register_lookup
 class In(Lookup):
     lookup_name = 'in'
     mongo_op = '$in'
@@ -60,40 +66,47 @@ class PatternLookup(Lookup):
         value = self.rhs
         if self.pattern:
             value = self.pattern % self.rhs
-        return {self.mongo_op: value, '$options', self.options}
+        return {self.mongo_op: value, '$options': self.options}
 
 
+@Field.register_lookup
 class Contains(PatternLookup):
     lookup_name = 'contains'
 
 
+@Field.register_lookup
 class IContains(Contains):
     lookup_name = 'icontains'
     options = 'i'
 
 
+@Field.register_lookup
 class StartsWith(PatternLookup):
     lookup_name = 'startswith'
     pattern = '^%s'
 
 
-class IStarsWith(StartsWith):
+@Field.register_lookup
+class IStartsWith(StartsWith):
     lookup_name = 'istartswith'
     options = 'i'
 
 
+@Field.register_lookup
 class EndsWith(PatternLookup):
     lookup_name = 'endswith'
     pattern = '%s$'
 
 
+@Field.register_lookup
 class IEndsWith(EndsWith):
     lookup_name = 'iendswith'
     options = 'i'
 
 
+@Field.register_lookup
 class IsNull(Lookup):
-    Lookup_name = 'isnull'
+    lookup_name = 'isnull'
     mongo_op = '$type'
 
     def process_rhs(self):
