47ef3b939a82bf6a20ba7068fcf09de89c2cbffe
Ryan Kroon <rykroon.tech@gmail.com>
2021-04-03 13:22:44 -0400

    created new utils/ folder

---
+++ mongopy/utils/functional.py
@@ -0,0 +1,34 @@
+class cached_property:
+    """
+    Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+    A cached property can be made out of an existing method:
+    (e.g. ``url = cached_property(get_absolute_url)``).
+    """
+
+    @staticmethod
+    def func(instance):
+        raise TypeError(
+            'Cannot use cached_property instance without calling '
+            '__set_name__() on it.'
+        )
+
+    def __init__(self, func):
+        self.real_func = func
+        self.__doc__ = getattr(func, '__doc__')
+
+    def __set_name__(self, owner, name):
+        self.name = name
+        self.func = self.real_func
+
+
+    def __get__(self, instance, cls=None):
+        """
+        Call the function and put the return value in instance.__dict__ so that
+        subsequent attribute access on the instance returns the cached value
+        instead of calling cached_property.__get__().
+        """
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
