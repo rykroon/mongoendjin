efc2b8e6abeaad3ee1e460695798cd9b5f0b381b
Ryan Kroon <rykroon.tech@gmail.com>
2021-04-04 10:15:57 -0400

    added Iterable classes and added lots of methods

--- mongopy/query.py
+++ mongopy/query.py
@@ -3,12 +3,96 @@ from mongopy.constants import LOOKUP_SEP
 from mongopy.query_utils import Q
 
 
+class BaseIterable:
+    pass
+
+
+class ModelIterable(BaseIterable):
+    pass
+
+
+class ValuesIterable(BaseIterable):
+    pass
+
+
+
 class QuerySet():
     
     def __init__(self, model=None, query=None, using=None):
         self.model = model
         self._db = using
         self._query = query or mongo.Query(self.model)
+        self._result_cache = None
+        self._iterable_class = ModelIterable
+
+    ########################
+    # PYTHON MAGIC METHODS #
+    ########################
+
+    def __len__(self):
+        self._fetch_all()
+        return len(self._result_cache)
+
+    def __iter__(self):
+        self._fetch_all()
+        return iter(self._result_cache)
+
+    def __bool__(self):
+        self._fetch_all()
+        return bool(self._result_cache)
+
+    def __getitem__(self, k):
+        if not isinstance(k, (int, slice)):
+            raise TypeError
+
+        #check negative indexing
+
+        if self._result_cache is not None:
+            return self._result_cache[k]
+
+        #additional logic goes here
+
+    
+    ####################################
+    # METHODS THAT DO DATABASE QUERIES #
+    ####################################
+
+    def count(self):
+        pass
+
+    def get(self, *args, **kwargs):
+        clone = self.filter(*args, **kwargs)
+        num = len(clone)
+        if num == 1:
+            return clone._result_cache[0]
+        
+        if not num:
+            raise Exception('DoesNotExist')
+
+        raise Exception('MultipleObjectsReturned')
+
+    def create(self, **kwargs):
+        pass
+
+    def first(self):
+        pass
+
+    def last(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def exists(self):
+        pass
+
+
+    ##################################################################
+    # PUBLIC METHODS THAT ALTER ATTRIBUTES AND RETURN A NEW QUERYSET #
+    ##################################################################
 
     def all(self):
         return self._chain()
@@ -30,10 +114,29 @@ class QuerySet():
         else:
             self._query.add_q(Q(*args, **kwargs))
 
+    def order_by(self, *field_names):
+        pass
+
+    def defer(self, *fields):
+        pass
+
+    def only(self, *fields):
+        pass
+
+    ###################
+    # PRIVATE METHODS #
+    ###################
+
     def _chain(self):
         obj = self._clone()
         return obj
 
     def _clone(self):
         c = self.__class__(model=self.model, query=self._query.chain(), using=self._db)
+        c._iterable_class = self._iterable_class
+        c._fields = self._fields
         return c
+
+    def _fetch_all(self):
+        if self._result_cache is None:
+            self._result_cache = list(self._iterable_class(self))
