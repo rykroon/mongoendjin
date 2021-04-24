9f2f94fa8173c759ee41f2f7486d5535bcbc769c
Ryan Kroon <rykroon.tech@gmail.com>
2021-04-24 08:25:25 -0400

    added all(), get_queryset(), and from_querset() methods

--- mongoendjin/db/models/manager.py
+++ mongoendjin/db/models/manager.py
@@ -1,11 +1,47 @@
-
+import inspect
+from mongoendjin.db.models.query import QuerySet
 
 class BaseManager:
+
+    auto_created = False
+
     def __init__(self):
         super().__init__()
         self.model = None
         self.name = None
         self._db = None
+
+    @classmethod
+    def _get_queryset_methods(cls, queryset_class):
+        def create_method(name, method):
+            def manager_method(self, *args, **kwargs):
+                return getattr(self.get_queryset(), name)(*args, **kwargs)
+            manager_method.__name__ = method.__name__
+            manager_method.__doc__ = method.__doc__
+            return manager_method
+
+        new_methods = {}
+        for name, method in inspect.getmembers(queryset_class, predicate=inspect.isfunction):
+            # Only copy missing methods.
+            if hasattr(cls, name):
+                continue
+            # Only copy public methods or methods with the attribute `queryset_only=False`.
+            queryset_only = getattr(method, 'queryset_only', None)
+            if queryset_only or (queryset_only is None and name.startswith('_')):
+                continue
+            # Copy the method onto the manager.
+            new_methods[name] = create_method(name, method)
+        return new_methods
+
+    @classmethod
+    def from_queryset(cls, queryset_class, class_name=None):
+        if class_name is None:
+            class_name = '%sFrom%s' % (cls.__name__, queryset_class.__name__)
+
+        return type(class_name, (cls,), {
+            '_queryset_class': queryset_class,
+            **cls._get_queryset_methods(queryset_class),
+        })
 
     def contribute_to_class(self, cls, name):
         self.name = self.name or name
@@ -15,8 +51,14 @@ class BaseManager:
 
         cls._meta.add_manager(self)
 
+    def get_queryset(self):
+        return self._queryset_class(model=self.model, using=self._db)
 
-class Manager(BaseManager):
+    def all(self):
+        return self.get_queryset()
+
+
+class Manager(BaseManager.from_queryset(QuerySet)):
     pass
 
 
